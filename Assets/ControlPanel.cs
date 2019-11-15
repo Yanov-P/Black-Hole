02e1fcec147d5aef20367afcbb5fd15241f9472f
Yanov-P <yanovpropeshtinktovich@gmail.com>
2019-11-15 16:36:54 +0300

    added movement

---
+++ Assets/ControlPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ControlPanel : MonoBehaviour, IEndDragHandler, IDragHandler, IBeginDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    //private Vector2 _offset = new Vector2();
+    //private Vector2 _currentOffset = new Vector2();
+
+    private Vector2 _screenSizes;
+    private Vector2 _currentOffset = new Vector2();
+    private Vector2 _startPos = new Vector2();
+
+    [SerializeField]
+    private PlayerMovement _playerMovement;
+
+    [SerializeField]
+    private const float MAX_CLICK_OFFSET = 20;
+
+    private void Start()
+    {
+        _screenSizes = new Vector2(Screen.width, Screen.height);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //_playerMovement.BeginDrag();
+        _startPos = eventData.position;
+        _playerMovement.BeginDrag(_currentOffset);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _currentOffset = eventData.position - _startPos;
+        _playerMovement.Move(_currentOffset);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _startPos = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if ((eventData.position - _startPos).magnitude < MAX_CLICK_OFFSET)
+        {
+            //click happened
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[SerializeField]
-    //private Transform _blackHole;
-    //private float _distanceToHole;
-    //private float _currentAngle = 0;
-    //private float _angularSpeed = 0.01f;
-    //void Start()
-    //{
-    //    _distanceToHole = new Vector2(Mathf.Abs(_blackHole.position.x - transform.position.x), Mathf.Abs(_blackHole.position.z - transform.position.z)).magnitude;
-    //}
+    [SerializeField]
+    private Transform _leftBottom;
 
-    //void Update()
-    //{
-    //    transform.position = new Vector3(_distanceToHole * Mathf.Sin(_currentAngle), transform.position.y, _distanceToHole * Mathf.Cos(_currentAngle));
-    //    _currentAngle += _angularSpeed;
-    //}
-    void Start()
+    [SerializeField]
+    private Transform _rightTop;
+
+    private Vector2 _startPos;
+    private Vector3 _startPosWorld;
+    private float _distToCamera;
+    private Vector2 _bounds;
+    private Vector2 _currOffset;
+
+    private void Start()
     {
-        Debug.Log("player movement");
+        _currOffset = new Vector2();
+        _distToCamera = Vector3.Distance(transform.position, Camera.main.transform.position);
+        _startPos = Camera.main.WorldToScreenPoint(transform.position);
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, _distToCamera));
+    }
+
+    public void BeginDrag(Vector2 offset)
+    {
+        _startPos = Camera.main.WorldToScreenPoint(transform.position);
+        _startPosWorld = _startPos;
+    }
+    public void Move(Vector2 offset)
+    {
+        Vector2 caclulatedPos = offset + _startPos;
+        bool xGood = caclulatedPos.x < Screen.width && caclulatedPos.x > 0;
+        bool yGood = caclulatedPos.y < Screen.height && caclulatedPos.y > 0;
+        if (xGood)
+        {
+            _currOffset.x = offset.x;
+        }
+        if (yGood)
+        {
+            _currOffset.y = offset.y;
+        }
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(_currOffset.x, _currOffset.y, _distToCamera) + _startPosWorld);
+
+    }
+
+    private void Update()
+    {
+        _rightTop.Translate(0.001f, 0, 0, Space.Self);
     }
 }
