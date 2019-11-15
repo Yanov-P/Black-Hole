dc90f9fd4fb662ec0326903b566316ac03284626
Haleralex <aleks.haler.work@gmail.com>
2019-11-15 16:11:40 +0300

    filled shooting script

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class ShootingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Pool
     {
-        
+        public string tag;
+        public GameObject prefab;
+        public Transform transform;
+        public int size;
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Singleton
+
+    public static ShootingScript Instance;
+
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    #endregion
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        MakePools();
+
+        InvokeRepeating("FullShoot", 0, 0.1f);
+    }
+
+    void MakePools()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+
+    }
+
+    void FullShoot()
+    {
+        foreach (Pool pool in pools)
+        {
+            Shoot(pool);
+        }
+    }
+
+    public GameObject Shoot(Pool pool)
+    {
+        if (!poolDictionary.ContainsKey(pool.tag))
+        {
+            Debug.LogWarning("Pool with tag" + pool.tag + " doesn't exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[pool.tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = pool.transform.position;
+        objectToSpawn.transform.rotation = pool.transform.rotation;
+
+        objectToSpawn.GetComponent<Rigidbody>().velocity = objectToSpawn.transform.TransformDirection(new Vector3(0, 0, -100));
+        poolDictionary[pool.tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
     }
 }
