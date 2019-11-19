4be99dcda17596e13f28cdcd5ede624a4fef2064
Haleralex <aleks.haler.work@gmail.com>
2019-11-19 12:04:08 +0300

    Laser gun

---
+++ Assets/Scripts/LaserGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour,  IWeapon
+{
+    public void MakePools()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void MakeSound()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetWeapon()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject Shoot(Pool pool)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartAnimation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartShooting()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopAnimation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopShooting()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
