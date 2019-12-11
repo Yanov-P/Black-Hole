eb1446d0cde549065aedffbf8e2666920489704d
aleks.haler <aleks.haler.work@gmail.com>
2019-12-11 13:15:37 +0300

    bitesspawn

---
+++ Assets/Scripts/BitesSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitesSpawner : MonoBehaviour
+{
+    Queue<GameObject> _poolOfBites;
+
+    [SerializeField]
+    List<Transform> _listOfTransforms;
+
+    [SerializeField]
+    GameObject _instance;
+
+    void Start()
+    {
+        FillPool();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartSpawn();
+
+        }
+    }
+    void StartSpawn()
+    {
+        CancelInvoke("Spawn");
+        InvokeRepeating("Spawn", 0, 1.0f);
+    }
+    void Spawn()
+    {
+        GameObject objectToSpawn = _poolOfBites.Dequeue();
+        objectToSpawn.transform.localScale = new Vector3(Random.Range(25, 35), Random.Range(25, 35), Random.Range(25, 35));
+        objectToSpawn.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        var number = Random.Range(0, _listOfTransforms.Count);
+        objectToSpawn.transform.position = _listOfTransforms[number].position;
+        objectToSpawn.gameObject.SetActive(true);
+        _poolOfBites.Enqueue(objectToSpawn);
+    }
+
+    public void FillPool()
+    {
+        _poolOfBites = new Queue<GameObject>();
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject obj = Instantiate(_instance);
+            obj.gameObject.SetActive(false);
+            _poolOfBites.Enqueue(obj);
+        }
+    }
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,12 +11,13 @@ public class CameraFollow : MonoBehaviour
     private float smoothSpeed = 0.125f;
     [SerializeField]
     private Vector3 offset;
-
+    
     void LateUpdate()
     {
+        
         Vector3 t = target.position;
         target.position = t;
-        Vector3 newTarget = new Vector3((t.x + 650) / 20.0f - 650, (t.y + 0) / 20.0f - 0, 0);
+        Vector3 newTarget = new Vector3((t.x + 650) / 20.0f - 650, (t.y + offset.y) / 20.0f - 0, 0);
         transform.LookAt(newTarget);
     }
 }
